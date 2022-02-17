27d0bb36da1f4b709e9858f162adab07ee297bfc
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-02-17 14:08:35 +0100

    Add file structure, products

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+// This file is your starting point (= since it's the index)
+// It will contain most of the logic, to prevent making a messy mix in the html
+
+// This line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// We are going to use session variables so we need to enable sessions
+session_start();
+
+
+
+// Use this function when you need to need an overview of these variables
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+// TODO: provide some products (you may overwrite the example)
+$products = [
+    ['name' => 'Schola Sarmenti - Cubardi - Red', 'price' => 15.3],
+    ['name' => 'Hans Greyl - Sauvignon Blanc - White', 'price' => 9.95],
+    ['name' => 'Schola Sarmenti - Opra - Rosé', 'price' => 14.5],
+];
+
+$totalValue = 0;
+
+function validate()
+{
+    // This function will send a list of invalid fields back
+    return [];
+}
+
+function handleForm()
+{
+    // TODO: form related tasks (step 1)
+    echo '<script>alert("Your order has been placed!")</script>';
+
+    // Validation (step 2)
+    $invalidFields = validate();
+    if (!empty($invalidFields)) {
+        // TODO: handle errors
+    } else {
+        // TODO: handle successful submission
+    }
+}
+
+// TODO: replace this if by an actual check
+$formSubmitted = false;
+if ($formSubmitted) {
+    handleForm();
+}
+
+require 'form-view.php';
+
+// var_dump($_GET);
